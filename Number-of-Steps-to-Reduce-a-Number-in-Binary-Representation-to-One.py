a3b127f1ae4e2b05bf2b584d7d426b634c650efb
apurvahegde2000 <63050355+apurvahegde2000@users.noreply.github.com>
2020-04-05 10:27:05 +0530

    Create Number-of-Steps-to-Reduce-a-Number-in-Binary-Representation-to-One.py

---
+++ Number-of-Steps-to-Reduce-a-Number-in-Binary-Representation-to-One.py
@@ -0,0 +1,58 @@
+'''
+Given a number s in their binary representation. Return the number of steps to reduce it to 1 under the following rules:
+
+If the current number is even, you have to divide it by 2.
+
+If the current number is odd, you have to add 1 to it.
+
+It's guaranteed that you can always reach to one for all testcases.
+Example 1:
+
+Input: s = "1101"
+Output: 6
+Explanation: "1101" corressponds to number 13 in their decimal representation.
+Step 1) 13 is odd, add 1 and obtain 14. 
+Step 2) 14 is even, divide by 2 and obtain 7.
+Step 3) 7 is odd, add 1 and obtain 8.
+Step 4) 8 is even, divide by 2 and obtain 4.  
+Step 5) 4 is even, divide by 2 and obtain 2. 
+Step 6) 2 is even, divide by 2 and obtain 1.  
+Example 2:
+
+Input: s = "10"
+Output: 1
+Explanation: "10" corressponds to number 2 in their decimal representation.
+Step 1) 2 is even, divide by 2 and obtain 1.  
+Example 3:
+
+Input: s = "1"
+Output: 0
+
+Explanation:First convert the binary number to a decimal number,then using a while loop keep adding 1 when the no becomes odd,
+and keep dividing by 2 if the number is even till the no becomes 1.
+'''
+class Solution(object):
+    def numSteps(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        b=list(map(int,s.split()))
+        #print(b)
+        d=0
+        i=0
+        while(b[0]!=0):
+            r=b[0]%10
+            d=d+r*pow(2,i)
+            b[0]=b[0]/10
+            i=i+1
+            
+        #print(d)
+        c=0
+        while(d!=1):
+            if(d%2==0):
+                d=d/2
+            else:
+                d=d+1
+            c=c+1
+        return c  
